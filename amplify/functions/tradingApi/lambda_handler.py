d38b89bb94ed45d7327b949ce576dfbfdcdb2194
Lrd0036 <lrd0036@auburn.edu>
2025-12-03 00:12:06 -0600

    Deleted amplify

--- amplify/functions/tradingApi/lambda_handler.py
+++
@@ -1,9 +0,0 @@
-# amplify/functions/tradingApi/lambda_handler.py
-# import awsgi  <-- REMOVE THIS
-import wsgi_sls as awsgi # Use the new package with an alias for minimal code change
-from app import app
-
-# This function is the entry point that AWS Lambda will call
-def handler(event, context):
-    # wsgi_sls.response is the equivalent function for the new package
-    return awsgi.response(app, event, context)
